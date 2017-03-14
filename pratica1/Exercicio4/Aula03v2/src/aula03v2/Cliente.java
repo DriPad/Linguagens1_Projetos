737e0f3c2cac016c315c1b9ed41407cc80e3b0aa
Adriana Maria Padilla <dri_padilla@hotmail.com>
2017-03-14 11:04:25 -0300

    Conta do cliente

---
+++ pratica1/Exercicio4/Aula03v2/src/aula03v2/Cliente.java
@@ -0,0 +1,6 @@
+package aula03v2;
+public class Cliente {
+    String nome;
+    String sobrenome;
+    String cpf;
+}
--- pratica1/Exercicio4/Aula03v2/src/aula03v2/Conta.java
+++ pratica1/Exercicio4/Aula03v2/src/aula03v2/Conta.java
@@ -1,24 +1,52 @@
 package aula03v2;
 public class Conta {
     int numero;
-    String titular;
-    float saldo;
-    String cpf;
+    Cliente titular;
+    double saldo;
+    
     
     void visualizarSaldo(){
         System.out.println("Saldo = " + this.saldo);
     }
     
-    void depositar(){
+    void depositar(double valor){
+        if (valor > 0){
+            this.saldo = this.saldo + valor;
+        }
         
     }
     
-    void sacar(){
+    void VisualizarConta (){
+       System.out.println("Nome do titular: " + this.titular.nome);
+       System.out.println("Sobrenome: " + this.titular.sobrenome);
+       System.out.println("Cpf: " + this.titular.cpf);
+       System.out.println("Saldo: " + this.saldo);
+          
+    }
+    
+    
+    
+    boolean sacar(double valor){
+        if (valor > this.saldo){
+            return false;
+        }
+        else {
+            this.saldo = this.saldo - valor;
+            return true;
+        }
         
     }
     
-    void transferirDinheiro(){
-        
+    boolean transferirPara(Conta x, double valor){
+        if (this.sacar(valor)){
+            x.depositar(valor);
+            System.out.println("Transferencia realizada");
+            return true;
+        }
+        else {
+            System.out.println("Transferencia não realizada");
+            return false;
+        }
     }
     
 }
