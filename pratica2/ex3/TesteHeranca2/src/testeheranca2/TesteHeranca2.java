601f7c89a4ef0b4102b4c4c0ad1db796d9aef828
Adriana Maria Padilla <dri_padilla@hotmail.com>
2017-04-25 11:03:00 -0300

    scanner

--- pratica2/ex3/TesteHeranca2/src/testeheranca2/TesteHeranca2.java
+++ pratica2/ex3/TesteHeranca2/src/testeheranca2/TesteHeranca2.java
@@ -1,9 +1,14 @@
 package testeheranca2;
+import java.util.Scanner;
 
 public class TesteHeranca2 {
 
     public static void main(String[] args) {
-       
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int i = scan.nextInt();
+        System.out.println("s= "+ s);
+        System.out.println("i= "+ i);
     }
     
 }
