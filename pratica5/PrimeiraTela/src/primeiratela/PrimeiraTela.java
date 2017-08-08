fce5688491947972dd043eeaa886f4ed6339f7c5
Adriana Maria Padilla <dri_padilla@hotmail.com>
2017-08-08 11:12:31 -0300

    InterGrafica

---
+++ pratica5/PrimeiraTela/src/primeiratela/PrimeiraTela.java
@@ -0,0 +1,19 @@
+package primeiratela;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Aluno 17
+ */
+public class PrimeiraTela {
+
+    
+    public static void main(String[] args) {
+        
+        TelaPrincipal telaP = new TelaPrincipal();
+        
+        telaP.setVisible(true);
+    }
+}
