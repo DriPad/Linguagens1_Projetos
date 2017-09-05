324757f535675e1702f784a7a7ba472f4842293b
Adriana Maria Padilla <dri_padilla@hotmail.com>
2017-09-05 11:16:02 -0300

    Excecoes

---
+++ exercicio/src/exercicio/Exercicio.java
@@ -0,0 +1,42 @@
+package exercicio;
+
+
+public class Exercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("inicio do main");
+        metodo1();
+        System.out.println("fim do main");
+    }
+
+    static void metodo1() {
+        System.out.println("inicio do metodo1");
+        //try {
+            metodo2();
+        //} catch (Exception e) {
+            //System.out.println("Tratamento " + e);
+        }
+        System.out.println("fim do metodo1");
+    }
+
+    static void metodo2() throws ArrayIndexOutOfBoundsException{
+        System.out.println("inicio do metodo2");
+        int[] array = new int[10];
+        try{
+            for (int i = 0; i <= 15; i++) {
+
+                array[i] = i;
+                System.out.println(i);
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("erro");
+                throw (e);
+            }
+        }
+    System.out.println("fim do metodo2");
+    }
+}// TODO code application logic here
+
